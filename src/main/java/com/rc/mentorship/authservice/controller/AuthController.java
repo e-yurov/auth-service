d140d451ee7f6895ce4c4741364993a805018b09
Evgeniy Yurov <jon172839@gmail.com>
2024-08-29 03:11:55 +0300

    Add swagger docs to AuthController

--- src/main/java/com/rc/mentorship/authservice/controller/AuthController.java
+++ src/main/java/com/rc/mentorship/authservice/controller/AuthController.java
@@ -2,7 +2,14 @@ package com.rc.mentorship.authservice.controller;
 
 import com.rc.mentorship.authservice.dto.request.RegisterRequest;
 import com.rc.mentorship.authservice.dto.response.UserResponse;
+import com.rc.mentorship.authservice.exception.details.ErrorDetails;
 import com.rc.mentorship.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "Аутентификация")
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(
+            summary = "Регистрация нового пользователя"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успешная регистрация пользователя",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Пользователь с данным email уже существует",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorDetails.class)
+                            )
+                    }
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest registerRequest) {
         return ResponseEntity.ok(authService.register(registerRequest));
